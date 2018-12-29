4e46f84b6581922c050fe61dc3c80aa2d35ffed8
IVIanuu <IVIanuu@gmail.com>
2018-12-29 16:26:55 +0100

    Docs

--- injekt/src/main/java/com/ivianuu/injekt/MultiBinding.kt
+++ injekt/src/main/java/com/ivianuu/injekt/MultiBinding.kt
@@ -16,24 +16,48 @@
 
 package com.ivianuu.injekt
 
+/**
+ * Wraps a [Set] of [Declaration]s
+ */
 data class MultiBindingSet<T : Any>(val set: Set<Declaration<T>>)
 
+/**
+ * Returns a [Set] of [T]s
+ */
 fun <T : Any> MultiBindingSet<T>.toSet(params: ParamsDefinition? = null): Set<T> =
     set.map { it.resolveInstance(params) }.toSet()
 
+/**
+ * Returns a [Set] of [Provider]s of [T]
+ */
 fun <T : Any> MultiBindingSet<T>.toProviderSet(defaultParams: ParamsDefinition? = null): Set<Provider<T>> =
     set.map { DeclarationProvider(it, defaultParams) }.toSet()
 
+/**
+ * Returns a [Set] of [Lazy]s of [T]
+ */
 fun <T : Any> MultiBindingSet<T>.toLazySet(params: ParamsDefinition? = null): Set<Lazy<T>> =
     set.map { lazy { it.resolveInstance(params) } }.toSet()
 
+/**
+ * Wraps a [Map] of [Declaration]s
+ */
 data class MultiBindingMap<K : Any, T : Any>(val map: Map<K, Declaration<T>>)
 
+/**
+ * Returns a [Map] of [K] and [T]s
+ */
 fun <K : Any, T : Any> MultiBindingMap<K, T>.toMap(params: ParamsDefinition? = null) =
     map.mapValues { it.value.resolveInstance(params) }
 
+/**
+ * Returns a [Map] of [K] and [Provider]s of [T]
+ */
 fun <K : Any, T : Any> MultiBindingMap<K, T>.toProviderMap(defaultParams: ParamsDefinition? = null) =
     map.mapValues { DeclarationProvider(it.value, defaultParams) }
 
+/**
+ * Returns a [Map] of [K] and [Lazy]s of [T]
+ */
 fun <K : Any, T : Any> MultiBindingMap<K, T>.toLazyMap(params: ParamsDefinition? = null) =
     map.mapValues { lazy { it.value.resolveInstance(params) } }
