d25922a58899a058f910962bb27d9d0291a3ede3
IVIanuu <IVIanuu@gmail.com>
2019-03-12 20:18:34 +0100

    Qualifier docs

--- injekt/src/main/java/com/ivianuu/injekt/Qualifier.kt
+++ injekt/src/main/java/com/ivianuu/injekt/Qualifier.kt
@@ -1,10 +1,16 @@
 package com.ivianuu.injekt
 
 /**
- * @author Manuel Wrage (IVIanuu)
+ * Interface to distinct [Binding]s of the same type
  */
 interface Qualifier
 
+/**
+ * A [Qualifier] which uses a [name]
+ */
 data class StringQualifier(val name: String) : Qualifier
 
+/**
+ * Returns a new [StringQualifier] for [name]
+ */
 fun named(name: String): StringQualifier = StringQualifier(name)
