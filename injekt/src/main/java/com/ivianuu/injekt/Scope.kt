f80794f308f307adabced92408d9aa79c94391dd
IVIanuu <IVIanuu@gmail.com>
2019-03-13 00:28:54 +0100

    Docs

--- injekt/src/main/java/com/ivianuu/injekt/Scope.kt
+++ injekt/src/main/java/com/ivianuu/injekt/Scope.kt
@@ -1,10 +1,13 @@
 package com.ivianuu.injekt
 
 /**
- * @author Manuel Wrage (IVIanuu)
+ * Scope marker
  */
 interface Scope
 
+/**
+ * A [Scope] which uses the [name] in [toString]
+ */
 open class NamedScope(val name: String) : Scope {
     override fun toString(): String = name
 }
