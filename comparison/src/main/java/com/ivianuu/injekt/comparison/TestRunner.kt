ea0417842607bed4844a3f4e049dd05e3fc4338e
IVIanuu <IVIanuu@gmail.com>
2019-04-15 20:04:43 +0200

    Comparison changes

--- comparison/src/main/java/com/ivianuu/injekt/comparison/TestRunner.kt
+++ comparison/src/main/java/com/ivianuu/injekt/comparison/TestRunner.kt
@@ -16,22 +16,20 @@
 
 package com.ivianuu.injekt.comparison
 
-import com.ivianuu.injekt.comparison.custom.CustomTest
-import com.ivianuu.injekt.comparison.dagger2.DaggerTest
 import com.ivianuu.injekt.comparison.injekt.InjektTest
 import com.ivianuu.injekt.comparison.katana.KatanaTest
 import com.ivianuu.injekt.comparison.kodein.KodeinTest
 import com.ivianuu.injekt.comparison.koin.KoinTest
 import org.nield.kotlinstatistics.median
 
-private const val ROUNDS = 1_000
+private const val ROUNDS = 10_000
 
 fun runInjectionTests() {
     println("Running $ROUNDS iterations. Please stand by...")
 
     val tests = listOf(
-        DaggerTest(),
-        CustomTest(),
+        //DaggerTest(),
+        //CustomTest(),
         KodeinTest(),
         KoinTest(),
         InjektTest(),
@@ -106,15 +104,49 @@ fun Map<String, Results>.print() {
     println("Setup:")
     println("Library | Average | Median | Min | Max")
     forEach { (name, results) ->
-        println("$name | ${results.setupAverage} | ${results.setupMedian} | ${results.setupMin} | ${results.setupMax}")
+        println(
+            "$name | " +
+                    "${results.setupAverage} | " +
+                    "${results.setupMedian} | " +
+                    "${results.setupMin} | " +
+                    "${results.setupMax}"
+        )
     }
+
+    println()
+
+    println("Best:")
+    println("Average | Median | Min | Max")
+    println(
+        "${minBy { it.value.setupAverage }?.key} | " +
+                "${minBy { it.value.setupMedian }?.key} | " +
+                "${minBy { it.value.setupMin }?.key} | " +
+                "${maxBy { it.value.setupMax }?.key}"
+    )
 
     println()
 
     println("Injection:")
     println("Library | Average | Median | Min | Max")
     forEach { (name, results) ->
-        println("$name | ${results.injectionAverage} | ${results.injectionMedian} | ${results.injectionMin} | ${results.injectionMax}")
+        println(
+            "$name | " +
+                    "${results.injectionAverage} | " +
+                    "${results.injectionMedian} | " +
+                    "${results.injectionMin} | " +
+                    "${results.injectionMax}"
+        )
     }
+
+    println()
+
+    println("Best:")
+    println("Average | Median | Min | Max")
+    println(
+        "${minBy { it.value.injectionAverage }?.key} | " +
+                "${minBy { it.value.injectionMedian }?.key} | " +
+                "${minBy { it.value.injectionMin }?.key} | " +
+                "${maxBy { it.value.injectionMax }?.key}"
+    )
 
 }
