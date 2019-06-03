d771fe3d2827ffd42f91c810cc2ed1e905d426fb
IVIanuu <IVIanuu@gmail.com>
2019-06-03 13:16:31 +0200

    Incremental processor

--- injekt-compiler/src/main/java/com/ivianuu/injekt/compiler/InjektProcessor.kt
+++ injekt-compiler/src/main/java/com/ivianuu/injekt/compiler/InjektProcessor.kt
@@ -16,12 +16,9 @@
 
 package com.ivianuu.injekt.compiler
 
-import com.google.auto.service.AutoService
 import com.ivianuu.processingx.steps.ProcessingStep
 import com.ivianuu.processingx.steps.StepProcessor
-import javax.annotation.processing.Processor
 
-@AutoService(Processor::class)
 class InjektProcessor : StepProcessor() {
     override fun initSteps(): Set<ProcessingStep> = setOf(CreatorStep())
 }
