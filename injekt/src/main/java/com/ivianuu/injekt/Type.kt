01d372bdb1c28cef87078a9a04d5226c437aeab9
IVIanuu <IVIanuu@gmail.com>
2019-06-11 12:06:59 +0200

    Primitive support ;((((

--- injekt/src/main/java/com/ivianuu/injekt/Type.kt
+++ injekt/src/main/java/com/ivianuu/injekt/Type.kt
@@ -83,16 +83,55 @@ internal fun <T> KType.asType(): Type<T> {
     )
 }
 
-fun <T> typeOf(raw: KClass<*>): Type<T> = Type(raw, false, emptyArray())
+fun <T> typeOf(raw: KClass<*>): Type<T> {
+    when (raw) {
+        Boolean::class, java.lang.Boolean::class.java -> return typeOf<Boolean>() as Type<T>
+        Byte::class, java.lang.Byte::class.java -> return typeOf<Byte>() as Type<T>
+        Char::class, java.lang.Character::class.java -> return typeOf<Char>() as Type<T>
+        Double::class, java.lang.Double::class.java -> return typeOf<Double>() as Type<T>
+        Float::class, java.lang.Float::class.java -> return typeOf<Float>() as Type<T>
+        Int::class, java.lang.Integer::class.java -> return typeOf<Int>() as Type<T>
+        Long::class, java.lang.Long::class.java -> return typeOf<Long>() as Type<T>
+        Short::class, java.lang.Short::class.java -> return typeOf<Short>() as Type<T>
+    }
+
+    return Type(raw, false, emptyArray())
+}
 
 fun <T> typeOf(raw: KClass<*>, vararg parameters: Type<*>): Type<T> =
     Type(raw, false, parameters)
 
-fun <T> typeOf(raw: KClass<*>, isNullable: Boolean): Type<T> =
-    Type(raw, isNullable, emptyArray())
+fun <T> typeOf(raw: KClass<*>, isNullable: Boolean): Type<T> {
+    if (isNullable) {
+        when (raw) {
+            Boolean::class, java.lang.Boolean::class -> return typeOf<Boolean?>() as Type<T>
+            Byte::class, java.lang.Byte::class -> return typeOf<Byte?>() as Type<T>
+            Char::class, java.lang.Character::class -> return typeOf<Char?>() as Type<T>
+            Double::class, java.lang.Double::class -> return typeOf<Double?>() as Type<T>
+            Float::class, java.lang.Float::class -> return typeOf<Float?>() as Type<T>
+            Int::class, java.lang.Integer::class -> return typeOf<Int?>() as Type<T>
+            Long::class, java.lang.Long::class -> return typeOf<Long?>() as Type<T>
+            Short::class, java.lang.Short::class -> return typeOf<Short?>() as Type<T>
+        }
+    } else {
+        when (raw) {
+            Boolean::class, java.lang.Boolean::class.java -> return typeOf<Boolean>() as Type<T>
+            Byte::class, java.lang.Byte::class.java -> return typeOf<Byte>() as Type<T>
+            Char::class, java.lang.Character::class.java -> return typeOf<Char>() as Type<T>
+            Double::class, java.lang.Double::class.java -> return typeOf<Double>() as Type<T>
+            Float::class, java.lang.Float::class.java -> return typeOf<Float>() as Type<T>
+            Int::class, java.lang.Integer::class.java -> return typeOf<Int>() as Type<T>
+            Long::class, java.lang.Long::class.java -> return typeOf<Long>() as Type<T>
+            Short::class, java.lang.Short::class.java -> return typeOf<Short>() as Type<T>
+        }
+    }
 
-fun <T> typeOf(raw: KClass<*>, isNullable: Boolean, vararg parameters: Type<*>): Type<T> =
-    Type(raw, isNullable, parameters)
+    return Type(raw, isNullable, emptyArray())
+}
+
+fun <T> typeOf(raw: KClass<*>, isNullable: Boolean, vararg parameters: Type<*>): Type<T> {
+    return Type(raw, isNullable, parameters)
+}
 
 fun <T> typeOf(type: java.lang.reflect.Type, isNullable: Boolean = false): Type<T> {
     if (type is WildcardType) {
@@ -100,19 +139,6 @@ fun <T> typeOf(type: java.lang.reflect.Type, isNullable: Boolean = false): Type<
             return typeOf(type.upperBounds.first(), isNullable)
         } else if (type.lowerBounds.isNotEmpty()) {
             return typeOf(type.lowerBounds.first(), isNullable)
-        }
-    }
-
-    if (!isNullable) {
-        when (type) {
-            java.lang.Boolean::class.java -> return typeOf<Boolean>() as Type<T>
-            java.lang.Byte::class.java -> return typeOf<Byte>() as Type<T>
-            java.lang.Character::class.java -> return typeOf<Char>() as Type<T>
-            java.lang.Double::class.java -> return typeOf<Double>() as Type<T>
-            java.lang.Float::class.java -> return typeOf<Float>() as Type<T>
-            java.lang.Integer::class.java -> return typeOf<Int>() as Type<T>
-            java.lang.Long::class.java -> return typeOf<Long>() as Type<T>
-            java.lang.Short::class.java -> return typeOf<Short>() as Type<T>
         }
     }
 
@@ -128,7 +154,29 @@ fun <T> typeOf(type: java.lang.reflect.Type, isNullable: Boolean = false): Type<
         )
     }
 
-
+    if (isNullable) {
+        when (type.typeName) {
+            "boolean" -> return typeOf<Boolean?>() as Type<T>
+            "byte" -> return typeOf<Byte?>() as Type<T>
+            "char" -> return typeOf<Char?>() as Type<T>
+            "double" -> return typeOf<Double?>() as Type<T>
+            "float" -> return typeOf<Float?>() as Type<T>
+            "int" -> return typeOf<Int?>() as Type<T>
+            "long" -> return typeOf<Long?>() as Type<T>
+            "short" -> return typeOf<Short?>() as Type<T>
+        }
+    } else {
+        when (type) {
+            java.lang.Boolean::class.java -> return typeOf<Boolean>() as Type<T>
+            java.lang.Byte::class.java -> return typeOf<Byte>() as Type<T>
+            java.lang.Character::class.java -> return typeOf<Char>() as Type<T>
+            java.lang.Double::class.java -> return typeOf<Double>() as Type<T>
+            java.lang.Float::class.java -> return typeOf<Float>() as Type<T>
+            java.lang.Integer::class.java -> return typeOf<Int>() as Type<T>
+            java.lang.Long::class.java -> return typeOf<Long>() as Type<T>
+            java.lang.Short::class.java -> return typeOf<Short>() as Type<T>
+        }
+    }
 
     return Type((type as Class<*>).kotlin, isNullable, emptyArray())
 }
--- injekt/src/test/java/com/ivianuu/injekt/TypeTest.kt
+++ injekt/src/test/java/com/ivianuu/injekt/TypeTest.kt
@@ -16,10 +16,189 @@
 
 package com.ivianuu.injekt
 
+import junit.framework.Assert.assertEquals
 import junit.framework.Assert.assertFalse
+import junit.framework.Assert.assertTrue
 import org.junit.Test
 
 class TypeTest {
+
+    private enum class Primitive {
+        BOOLEAN {
+            override val nonNullReified: Type<*> = typeOf<Boolean>()
+            override val nullableReified: Type<*> = typeOf<Boolean?>()
+            override val nonNullUnboxedClass: Type<*> = typeOf<Boolean>(Boolean::class)
+            override val nullableUnboxedClass: Type<*> = typeOf<Boolean?>(Boolean::class, true)
+            override val nonNullBoxedClass: Type<*> = typeOf<Boolean>(java.lang.Boolean::class)
+            override val nullableBoxedClass: Type<*> =
+                typeOf<Boolean?>(java.lang.Boolean::class, true)
+            override val nonNullUnboxedJavaType: Type<*> = typeOf<Boolean>(Boolean::class.java)
+            override val nullableUnboxedJavaType: Type<*> =
+                typeOf<Boolean?>(Boolean::class.java, true)
+            override val nonNullBoxedJavaType: Type<*> =
+                typeOf<Boolean>(java.lang.Boolean::class.java)
+            override val nullableBoxedJavaType: Type<*> =
+                typeOf<Boolean?>(java.lang.Boolean::class.java, true)
+        },
+        BYTE {
+            override val nonNullReified: Type<*> = typeOf<Byte>()
+            override val nullableReified: Type<*> = typeOf<Byte?>()
+            override val nonNullUnboxedClass: Type<*> = typeOf<Byte>(Byte::class)
+            override val nullableUnboxedClass: Type<*> = typeOf<Byte?>(Byte::class, true)
+            override val nonNullBoxedClass: Type<*> = typeOf<Byte>(java.lang.Byte::class)
+            override val nullableBoxedClass: Type<*> = typeOf<Byte?>(java.lang.Byte::class, true)
+            override val nonNullUnboxedJavaType: Type<*> = typeOf<Byte>(Byte::class.java)
+            override val nullableUnboxedJavaType: Type<*> = typeOf<Byte?>(Byte::class.java, true)
+            override val nonNullBoxedJavaType: Type<*> = typeOf<Byte>(java.lang.Byte::class.java)
+            override val nullableBoxedJavaType: Type<*> =
+                typeOf<Byte?>(java.lang.Byte::class.java, true)
+        },
+        CHAR {
+            override val nonNullReified: Type<*> = typeOf<Char>()
+            override val nullableReified: Type<*> = typeOf<Char?>()
+            override val nonNullUnboxedClass: Type<*> = typeOf<Char>(Char::class)
+            override val nullableUnboxedClass: Type<*> = typeOf<Char?>(Char::class, true)
+            override val nonNullBoxedClass: Type<*> = typeOf<Char>(java.lang.Character::class)
+            override val nullableBoxedClass: Type<*> =
+                typeOf<Char?>(java.lang.Character::class, true)
+            override val nonNullUnboxedJavaType: Type<*> = typeOf<Char>(Char::class.java)
+            override val nullableUnboxedJavaType: Type<*> = typeOf<Char?>(Char::class.java, true)
+            override val nonNullBoxedJavaType: Type<*> =
+                typeOf<Char>(java.lang.Character::class.java)
+            override val nullableBoxedJavaType: Type<*> =
+                typeOf<Char?>(java.lang.Character::class.java, true)
+        },
+        DOUBLE {
+            override val nonNullReified: Type<*> = typeOf<Double>()
+            override val nullableReified: Type<*> = typeOf<Double?>()
+            override val nonNullUnboxedClass: Type<*> = typeOf<Double>(Double::class)
+            override val nullableUnboxedClass: Type<*> = typeOf<Double?>(Double::class, true)
+            override val nonNullBoxedClass: Type<*> = typeOf<Double>(java.lang.Double::class)
+            override val nullableBoxedClass: Type<*> =
+                typeOf<Double?>(java.lang.Double::class, true)
+            override val nonNullUnboxedJavaType: Type<*> = typeOf<Double>(Double::class.java)
+            override val nullableUnboxedJavaType: Type<*> =
+                typeOf<Double?>(Double::class.java, true)
+            override val nonNullBoxedJavaType: Type<*> =
+                typeOf<Double>(java.lang.Double::class.java)
+            override val nullableBoxedJavaType: Type<*> =
+                typeOf<Double?>(java.lang.Double::class.java, true)
+        },
+        FLOAT {
+            override val nonNullReified: Type<*> = typeOf<Float>()
+            override val nullableReified: Type<*> = typeOf<Float?>()
+            override val nonNullUnboxedClass: Type<*> = typeOf<Float>(Float::class)
+            override val nullableUnboxedClass: Type<*> = typeOf<Float?>(Float::class, true)
+            override val nonNullBoxedClass: Type<*> = typeOf<Float>(java.lang.Float::class)
+            override val nullableBoxedClass: Type<*> = typeOf<Float?>(java.lang.Float::class, true)
+            override val nonNullUnboxedJavaType: Type<*> = typeOf<Float>(Float::class.java)
+            override val nullableUnboxedJavaType: Type<*> = typeOf<Float?>(Float::class.java, true)
+            override val nonNullBoxedJavaType: Type<*> = typeOf<Float>(java.lang.Float::class.java)
+            override val nullableBoxedJavaType: Type<*> =
+                typeOf<Float?>(java.lang.Float::class.java, true)
+        },
+        INT {
+            override val nonNullReified: Type<*> = typeOf<Int>()
+            override val nullableReified: Type<*> = typeOf<Int?>()
+            override val nonNullUnboxedClass: Type<*> = typeOf<Int>(Int::class)
+            override val nullableUnboxedClass: Type<*> = typeOf<Int?>(Int::class, true)
+            override val nonNullBoxedClass: Type<*> = typeOf<Int>(java.lang.Integer::class)
+            override val nullableBoxedClass: Type<*> = typeOf<Int?>(java.lang.Integer::class, true)
+            override val nonNullUnboxedJavaType: Type<*> = typeOf<Int>(Int::class.java)
+            override val nullableUnboxedJavaType: Type<*> = typeOf<Int?>(Int::class.java, true)
+            override val nonNullBoxedJavaType: Type<*> = typeOf<Int>(java.lang.Integer::class.java)
+            override val nullableBoxedJavaType: Type<*> =
+                typeOf<Int?>(java.lang.Integer::class.java, true)
+        },
+        LONG {
+            override val nonNullReified: Type<*> = typeOf<Long>()
+            override val nullableReified: Type<*> = typeOf<Long?>()
+            override val nonNullUnboxedClass: Type<*> = typeOf<Long>(Long::class)
+            override val nullableUnboxedClass: Type<*> = typeOf<Long?>(Long::class, true)
+            override val nonNullBoxedClass: Type<*> = typeOf<Long>(java.lang.Long::class)
+            override val nullableBoxedClass: Type<*> = typeOf<Long?>(java.lang.Long::class, true)
+            override val nonNullUnboxedJavaType: Type<*> = typeOf<Long>(Long::class.java)
+            override val nullableUnboxedJavaType: Type<*> = typeOf<Long?>(Long::class.java, true)
+            override val nonNullBoxedJavaType: Type<*> = typeOf<Long>(java.lang.Long::class.java)
+            override val nullableBoxedJavaType: Type<*> =
+                typeOf<Long?>(java.lang.Long::class.java, true)
+        },
+        SHORT {
+            override val nonNullReified: Type<*> = typeOf<Short>()
+            override val nullableReified: Type<*> = typeOf<Short?>()
+            override val nonNullUnboxedClass: Type<*> = typeOf<Short>(Short::class)
+            override val nullableUnboxedClass: Type<*> = typeOf<Short?>(Short::class, true)
+            override val nonNullBoxedClass: Type<*> = typeOf<Short>(java.lang.Short::class)
+            override val nullableBoxedClass: Type<*> = typeOf<Short?>(java.lang.Short::class, true)
+            override val nonNullUnboxedJavaType: Type<*> = typeOf<Short>(Short::class.java)
+            override val nullableUnboxedJavaType: Type<*> = typeOf<Short?>(Short::class.java, true)
+            override val nonNullBoxedJavaType: Type<*> = typeOf<Short>(java.lang.Short::class.java)
+            override val nullableBoxedJavaType: Type<*> =
+                typeOf<Short?>(java.lang.Short::class.java, true)
+        };
+
+        abstract val nonNullReified: Type<*>
+        abstract val nullableReified: Type<*>
+        abstract val nonNullUnboxedClass: Type<*>
+        abstract val nullableUnboxedClass: Type<*>
+        abstract val nonNullBoxedClass: Type<*>
+        abstract val nullableBoxedClass: Type<*>
+        abstract val nonNullUnboxedJavaType: Type<*>
+        abstract val nullableUnboxedJavaType: Type<*>
+        abstract val nonNullBoxedJavaType: Type<*>
+        abstract val nullableBoxedJavaType: Type<*>
+    }
+
+    @Test
+    fun testPrimitiveMapping() {
+        Primitive.values().forEach { type ->
+            val nonNullReified = type.nonNullReified
+            val nullableReified = type.nullableReified
+
+            val nonNullUnboxedClass = type.nonNullUnboxedClass
+            val nullableUnboxedClass = type.nullableUnboxedClass
+
+            val nonNullBoxedClass = type.nonNullBoxedClass
+            val nullableBoxedClass = type.nullableBoxedClass
+
+            val nonNullUnboxedJavaType = type.nonNullUnboxedJavaType
+            val nullableUnboxedJavaType = type.nullableUnboxedJavaType
+
+            val nonNullBoxedJavaType = type.nonNullBoxedJavaType
+            val nullableBoxedJavaType = type.nullableBoxedJavaType
+
+            val pairs = listOf(
+                nonNullReified to nullableReified,
+                nonNullUnboxedClass to nullableUnboxedClass,
+                nonNullBoxedClass to nullableBoxedClass,
+                nonNullUnboxedJavaType to nullableUnboxedJavaType,
+                nonNullBoxedJavaType to nullableBoxedJavaType
+            )
+
+            pairs.forEach { (nonNull, nullable) -> assertTrue(nonNull != nullable) }
+
+            val nonNulls = pairs.map { it.first }
+
+            for (i in 0 until nonNulls.size) {
+                for (j in i + 1 until nonNulls.size) {
+                    val a = nonNulls[i]
+                    val b = nonNulls[j]
+                    assertEquals(a, b)
+                }
+            }
+
+            val nulls = pairs.map { it.second }
+
+            for (i in 0 until nulls.size) {
+                for (j in i + 1 until nulls.size) {
+                    val a = nulls[i]
+                    val b = nulls[j]
+                    assertEquals(a, b)
+                }
+            }
+
+        }
+    }
 
     @Test
     fun testParameterDistinction() {
