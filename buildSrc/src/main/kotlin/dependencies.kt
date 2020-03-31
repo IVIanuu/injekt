caf2c70d27f36e416072c383f50218ca763b5149
ivianuu <ivianuu@gmail.com>
2020-03-31 16:01:45 +0200

    Fix injekt fragment factory

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -32,7 +32,7 @@ object Build {
 object Publishing {
     const val groupId = "com.ivianuu.injekt"
     const val vcsUrl = "https://github.com/IVIanuu/injekt"
-    const val version = "${Build.versionName}-dev120"
+    const val version = "${Build.versionName}-dev121"
 }
 
 object Deps {
--- injekt-android/src/main/kotlin/com/ivianuu/injekt/android/FragmentInjection.kt
+++ injekt-android/src/main/kotlin/com/ivianuu/injekt/android/FragmentInjection.kt
@@ -32,6 +32,7 @@ import com.ivianuu.injekt.QualifierMarker
 import com.ivianuu.injekt.alias
 import com.ivianuu.injekt.common.map
 import com.ivianuu.injekt.factory
+import com.ivianuu.injekt.invoke
 import com.ivianuu.injekt.keyOf
 
 @ApplicationScope
