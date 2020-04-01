d4e174208b0cb559f51960e78254a3c9b13ae392
ivianuu <ivianuu@gmail.com>
2020-04-01 21:23:06 +0200

    Made BoundBehavior a sealed class

--- injekt/src/main/kotlin/com/ivianuu/injekt/Bound.kt
+++ injekt/src/main/kotlin/com/ivianuu/injekt/Bound.kt
@@ -19,7 +19,7 @@ package com.ivianuu.injekt
 /**
  * Ensures instances will be resolved in the specified component
  */
-open class BoundBehavior(val scope: Scope? = null) : Behavior.Element {
+sealed class BoundBehavior(val scope: Scope? = null) : Behavior.Element {
     override fun <T> apply(provider: BindingProvider<T>): BindingProvider<T> =
         BoundProvider(scope, provider)
 
