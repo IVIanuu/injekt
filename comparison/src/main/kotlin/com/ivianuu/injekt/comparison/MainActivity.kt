6ac13ce5e7cdc503b1d57b785ba1d307a2590c5d
ivianuu <ivianuu@gmail.com>
2020-04-03 00:09:50 +0200

    Comparison additions

--- comparison/src/main/kotlin/com/ivianuu/injekt/comparison/MainActivity.kt
+++ comparison/src/main/kotlin/com/ivianuu/injekt/comparison/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.ivianuu.injekt.comparison
 
 import android.app.Activity
+import android.os.Bundle
 
-class MainActivity : Activity()
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        main()
+    }
+
+}
--- comparison/src/main/kotlin/com/ivianuu/injekt/comparison/TestRunner.kt
+++ comparison/src/main/kotlin/com/ivianuu/injekt/comparison/TestRunner.kt
@@ -21,7 +21,6 @@ import com.ivianuu.injekt.comparison.custom.CustomTest
 import com.ivianuu.injekt.comparison.dagger.DaggerTest
 import com.ivianuu.injekt.comparison.dagger2.Dagger2Test
 import com.ivianuu.injekt.comparison.dagger2reflect.Dagger2ReflectTest
-import com.ivianuu.injekt.comparison.guice.GuiceTest
 import com.ivianuu.injekt.comparison.injekt.InjektTest
 import com.ivianuu.injekt.comparison.katana.KatanaTest
 import com.ivianuu.injekt.comparison.kodein.KodeinTest
@@ -51,7 +50,7 @@ fun runAllInjectionTests(config: Config = defaultConfig) {
             DaggerTest,
             Dagger2Test,
             Dagger2ReflectTest,
-            GuiceTest,
+            //GuiceTest,
             InjektTest,
             KatanaTest,
             KodeinTest,
