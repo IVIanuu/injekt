e116603460295d8594e7e3bc6f3370744b81162b
ivianuu <ivianuu@gmail.com>
2020-04-17 13:46:34 +0200

    Testing changes

--- comparison/src/main/kotlin/com/ivianuu/injekt/comparison/MainActivity.kt
+++ comparison/src/main/kotlin/com/ivianuu/injekt/comparison/MainActivity.kt
@@ -4,13 +4,15 @@ import android.app.Activity
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.FrameLayout
+import com.ivianuu.injekt.comparison.base.defaultConfig
+import com.ivianuu.injekt.comparison.base.runInjectionTests
+import com.ivianuu.injekt.comparison.injekt.InjektTest
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //injekt { initializeEndpoint() }
-        //runInjectionTests(InjektTest, config = defaultConfig.copy(rounds = 1000))
+        runInjectionTests(InjektTest, config = defaultConfig.copy(rounds = 1000))
         setContentView(FrameLayout(this).apply { setBackgroundColor(Color.BLACK) })
 
     }
