d435b09da560f4ce01e27f1ede72e2dc948f26f8
ivianuu <ivianuu@gmail.com>
2020-04-19 15:06:23 +0200

    Require metadata values

--- injekt-core/src/main/kotlin/com/ivianuu/injekt/internal/ComponentMetadata.kt
+++ injekt-core/src/main/kotlin/com/ivianuu/injekt/internal/ComponentMetadata.kt
@@ -2,6 +2,6 @@ package com.ivianuu.injekt.internal
 
 @Target(AnnotationTarget.CLASS)
 annotation class ComponentMetadata(
-    val scopes: Array<String> = [],
-    val bindings: Array<String> = []
+    val scopes: Array<String>,
+    val bindings: Array<String>
 )
--- injekt-core/src/main/kotlin/com/ivianuu/injekt/internal/ModuleMetadata.kt
+++ injekt-core/src/main/kotlin/com/ivianuu/injekt/internal/ModuleMetadata.kt
@@ -2,8 +2,8 @@ package com.ivianuu.injekt.internal
 
 @Target(AnnotationTarget.CLASS)
 annotation class ModuleMetadata(
-    val scopes: Array<String> = [],
-    val parents: Array<String> = [],
-    val bindings: Array<String> = [],
-    val includedModules: Array<String> = []
+    val scopes: Array<String>,
+    val parents: Array<String>,
+    val bindings: Array<String>,
+    val includedModules: Array<String>
 )
