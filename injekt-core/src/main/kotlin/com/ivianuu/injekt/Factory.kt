674dc351f4fdcc4b8145603b196bed350847015b
ivianuu <ivianuu@gmail.com>
2020-05-05 01:25:51 +0200

    Removed todo

--- injekt-core/src/main/kotlin/com/ivianuu/injekt/Factory.kt
+++ injekt-core/src/main/kotlin/com/ivianuu/injekt/Factory.kt
@@ -3,8 +3,6 @@ package com.ivianuu.injekt
 @Target(AnnotationTarget.FUNCTION)
 annotation class Factory
 
-// todo implement
-
 @Qualifier
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.TYPE)
 annotation class ChildFactory
