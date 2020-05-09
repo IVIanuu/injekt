7f53f972897436a4b6ccc8dc69b3b6d8940e2a70
ivianuu <ivianuu@gmail.com>
2020-05-09 18:24:54 +0200

    Clean

--- injekt-compiler-plugin/src/main/kotlin/com/ivianuu/injekt/compiler/InjektPlugin.kt
+++ injekt-compiler-plugin/src/main/kotlin/com/ivianuu/injekt/compiler/InjektPlugin.kt
@@ -22,9 +22,12 @@ import com.ivianuu.injekt.compiler.analysis.InjektStorageContainerContributor
 import com.ivianuu.injekt.compiler.transform.InjektIrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.com.intellij.mock.MockProject
+import org.jetbrains.kotlin.com.intellij.openapi.extensions.Extensions
+import org.jetbrains.kotlin.com.intellij.openapi.project.Project
 import org.jetbrains.kotlin.compiler.plugin.ComponentRegistrar
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.extensions.StorageComponentContainerContributor
+import org.jetbrains.kotlin.resolve.diagnostics.DiagnosticSuppressor
 
 @AutoService(ComponentRegistrar::class)
 class InjektComponentRegistrar : ComponentRegistrar {
@@ -40,6 +43,18 @@ class InjektComponentRegistrar : ComponentRegistrar {
             project,
             InjektIrGenerationExtension()
         )
-        InjektDiagnosticSuppressor.registerExtension(project, InjektDiagnosticSuppressor())
+        registerDiagnosticSuppressorExtension(
+            project,
+            InjektDiagnosticSuppressor()
+        )
+    }
+
+    private fun registerDiagnosticSuppressorExtension(
+        @Suppress("UNUSED_PARAMETER") project: Project,
+        extension: DiagnosticSuppressor
+    ) {
+        @Suppress("DEPRECATION")
+        Extensions.getRootArea().getExtensionPoint(DiagnosticSuppressor.EP_NAME)
+            .registerExtension(extension)
     }
 }
--- injekt-compiler-plugin/src/main/kotlin/com/ivianuu/injekt/compiler/analysis/InjektDiagnosticSuppressor.kt
+++ injekt-compiler-plugin/src/main/kotlin/com/ivianuu/injekt/compiler/analysis/InjektDiagnosticSuppressor.kt
@@ -1,7 +1,5 @@
 package com.ivianuu.injekt.compiler.analysis
 
-import org.jetbrains.kotlin.com.intellij.openapi.extensions.Extensions
-import org.jetbrains.kotlin.com.intellij.openapi.project.Project
 import org.jetbrains.kotlin.diagnostics.Diagnostic
 import org.jetbrains.kotlin.diagnostics.Errors
 import org.jetbrains.kotlin.psi.KtClass
@@ -9,18 +7,7 @@ import org.jetbrains.kotlin.psi.psiUtil.getParentOfType
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.diagnostics.DiagnosticSuppressor
 
-open class InjektDiagnosticSuppressor : DiagnosticSuppressor {
-
-    companion object {
-        fun registerExtension(
-            @Suppress("UNUSED_PARAMETER") project: Project,
-            extension: DiagnosticSuppressor
-        ) {
-            @Suppress("DEPRECATION")
-            Extensions.getRootArea().getExtensionPoint(DiagnosticSuppressor.EP_NAME)
-                .registerExtension(extension)
-        }
-    }
+class InjektDiagnosticSuppressor : DiagnosticSuppressor {
 
     override fun isSuppressed(diagnostic: Diagnostic): Boolean {
         return isSuppressed(diagnostic, null)
