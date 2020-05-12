6fccf0c1e88d982781b668021287a4b7363c088f
ivianuu <ivianuu@gmail.com>
2020-05-12 16:02:10 +0200

    Test clean

--- injekt-compiler-plugin/src/test/kotlin/com/ivianuu/injekt/compiler/InlineTest.kt
+++ injekt-compiler-plugin/src/test/kotlin/com/ivianuu/injekt/compiler/InlineTest.kt
@@ -172,4 +172,26 @@ class InlineTest {
     """
     )
 
+    @Test
+    fun testSimpleInlineModule() = codegen(
+        """
+        @Module 
+        inline fun <T : Any, S> inlinedModule(definition: ProviderDefinition<S>) {
+            transient<T>()
+            inlinedModule2(definition)
+        }
+        
+        @Module 
+        inline fun <P> inlinedModule2(definition: ProviderDefinition<P>) {
+            transient(definition)
+        }
+        
+        @Factory
+        fun factory(): Bar { 
+            inlinedModule<Foo, Bar> { Bar(get()) }
+            return createInstance()
+        }
+    """
+    )
+
 }
