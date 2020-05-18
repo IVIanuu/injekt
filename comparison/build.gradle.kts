a47cb737909e761757ceaa96653d14aa72a822d4
ivianuu <ivianuu@gmail.com>
2020-05-18 12:47:58 +0200

    Fix comparison

--- comparison/build.gradle.kts
+++ comparison/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(Deps.guice)
 
     implementation(project(":injekt-core"))
+    implementation(project(":injekt-common"))
     kotlinCompilerPluginClasspath(project(":injekt-compiler-plugin"))
 
     implementation(Deps.katana)
