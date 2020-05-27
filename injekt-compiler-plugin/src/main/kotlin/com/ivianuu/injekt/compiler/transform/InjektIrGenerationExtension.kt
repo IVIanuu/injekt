7dac985cd10520b79911938f0104736956ff1704
ivianuu <ivianuu@gmail.com>
2020-05-28 01:47:30 +0200

    Alias and inline class tests

--- injekt-compiler-plugin/src/main/kotlin/com/ivianuu/injekt/compiler/transform/InjektIrGenerationExtension.kt
+++ injekt-compiler-plugin/src/main/kotlin/com/ivianuu/injekt/compiler/transform/InjektIrGenerationExtension.kt
@@ -18,7 +18,6 @@ package com.ivianuu.injekt.compiler.transform
 
 import com.ivianuu.injekt.compiler.androidEnabled
 import com.ivianuu.injekt.compiler.compositionsEnabled
-import com.ivianuu.injekt.compiler.dumpSrc
 import com.ivianuu.injekt.compiler.transform.android.AndroidEntryPointTransformer
 import com.ivianuu.injekt.compiler.transform.composition.BindingEffectTransformer
 import com.ivianuu.injekt.compiler.transform.composition.CompositionAggregateGenerator
@@ -109,7 +108,7 @@ class InjektIrGenerationExtension(
         // patch metadata
         TmpMetadataPatcher(pluginContext).lower(moduleFragment)
 
-        println(moduleFragment.dumpSrc())
+        //println(moduleFragment.dumpSrc())
     }
 
 }
