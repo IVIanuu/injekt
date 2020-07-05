365aef675ad70d24df13da62e98c159f6c77bcf5
ivianuu <ivianuu@gmail.com>
2020-07-05 16:03:49 +0200

    ..

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -31,7 +31,7 @@ object Build {
 object Publishing {
     const val groupId = "com.ivianuu.injekt"
     const val vcsUrl = "https://github.com/IVIanuu/injekt"
-    const val version = "${Build.versionName}-dev197"
+    const val version = "${Build.versionName}-dev199"
 }
 
 object Deps {
