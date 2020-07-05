929d82972f0a7fe5b545f570cb42201e1f1f2290
ivianuu <ivianuu@gmail.com>
2020-07-05 20:24:02 +0200

    Version bump

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -31,7 +31,7 @@ object Build {
 object Publishing {
     const val groupId = "com.ivianuu.injekt"
     const val vcsUrl = "https://github.com/IVIanuu/injekt"
-    const val version = "${Build.versionName}-dev201"
+    const val version = "${Build.versionName}-dev202"
 }
 
 object Deps {
