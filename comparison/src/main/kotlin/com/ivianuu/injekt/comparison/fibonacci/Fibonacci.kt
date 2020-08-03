a70900702224c6c3eb91be8c226a50a0b58bec55
ivianuu <ivianuu@gmail.com>
2020-08-03 19:13:49 +0200

    Do not scope fibs

--- comparison/src/main/kotlin/com/ivianuu/injekt/comparison/fibonacci/Fibonacci.kt
+++ comparison/src/main/kotlin/com/ivianuu/injekt/comparison/fibonacci/Fibonacci.kt
@@ -27,64 +27,50 @@ fun fib1() = Fib1()
 
 class Fib2 @Inject constructor()
 
-@Given(ApplicationScoped::class)
+@Given
 fun fib2() = Fib2()
-
-@Singleton
 class Fib3 @Inject constructor(
     val fibM1: Fib2,
     val fibM2: Fib1
 )
 
-@Given(ApplicationScoped::class)
+@Given
 fun fib3() = Fib3(given(), given())
-
-@Singleton
 class Fib4 @Inject constructor(
     val fibM1: Fib3,
     val fibM2: Fib2
 )
 
-@Given(ApplicationScoped::class)
+@Given
 fun fib4() = Fib4(given(), given())
-
-@Singleton
 class Fib5 @Inject constructor(
     val fibM1: Fib4,
     val fibM2: Fib3
 )
 
-@Given(ApplicationScoped::class)
+@Given
 fun fib5() = Fib5(given(), given())
-
-@Singleton
 class Fib6 @Inject constructor(
     val fibM1: Fib5,
     val fibM2: Fib4
 )
 
-@Given(ApplicationScoped::class)
+@Given
 fun fib6() = Fib6(given(), given())
-
-@Singleton
 class Fib7 @Inject constructor(
     val fibM1: Fib6,
     val fibM2: Fib5
 )
 
-@Given(ApplicationScoped::class)
+@Given
 fun fib7() = Fib7(given(), given())
-
-@Singleton
 class Fib8 @Inject constructor(
     val fibM1: Fib7,
     val fibM2: Fib6
 )
 
-@Given(ApplicationScoped::class)
+@Given
 fun fib8() = Fib8(given(), given())
-
-@Singleton
 class Fib9 @Inject constructor(
     val fibM1: Fib8,
     val fibM2: Fib7
