080b5fc1dd39761f034b63ed36b4ee66cd9a46e3
ivianuu <ivianuu@gmail.com>
2020-08-17 11:43:09 +0200

    Clean

--- injekt-core/src/main/kotlin/com/ivianuu/injekt/Context.kt
+++ injekt-core/src/main/kotlin/com/ivianuu/injekt/Context.kt
@@ -5,17 +5,9 @@ import com.ivianuu.injekt.internal.injektIntrinsic
 @Target(AnnotationTarget.CLASS)
 annotation class Context
 
-fun <T> context(
-    vararg inputs: Any?
-): T = injektIntrinsic()
+fun <T> context(vararg inputs: Any?): T = injektIntrinsic()
 
 @Reader
-fun <T> childContext(
-    vararg inputs: Any?
-): T = injektIntrinsic()
+fun <T> childContext(vararg inputs: Any?): T = injektIntrinsic()
 
-inline fun <R> Any.runReader(
-    block: @Reader () -> R
-): R = injektIntrinsic()
-
-fun <T> givenSet(): T = error("")
+inline fun <R> Any.runReader(block: @Reader () -> R): R = injektIntrinsic()
