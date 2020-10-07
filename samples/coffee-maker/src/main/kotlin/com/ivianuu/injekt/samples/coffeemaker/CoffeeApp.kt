53de152e13d19740133f2c4a2c896b56a8b3e6e6
ivianuu <ivianuu@gmail.com>
2020-10-07 23:33:13 +0200

    Cleaned coffee sample

--- samples/coffee-maker/src/main/kotlin/com/ivianuu/injekt/samples/coffeemaker/CoffeeApp.kt
+++ samples/coffee-maker/src/main/kotlin/com/ivianuu/injekt/samples/coffeemaker/CoffeeApp.kt
@@ -1,6 +1,8 @@
 package com.ivianuu.injekt.samples.coffeemaker
 
+import com.ivianuu.injekt.Component
 import com.ivianuu.injekt.FunBinding
+import com.ivianuu.injekt.ImplBinding
 
 fun main() {
     val component = CoffeeComponentImpl()
@@ -13,4 +15,46 @@ fun brewCoffee(heater: Heater, pump: Pump) {
     pump.pump()
     println(" [_]P coffee! [_]P ")
     heater.off()
+}
+
+
+@Component
+abstract class CoffeeComponent {
+    abstract val brewCoffee: brewCoffee
+}
+
+interface Heater {
+    fun on()
+    fun off()
+    val isHot: Boolean
+}
+
+@ImplBinding(CoffeeComponent::class)
+class ElectricHeater : Heater {
+    private var heating: Boolean = false
+
+    override fun on() {
+        println("~ ~ ~ heating ~ ~ ~")
+        heating = true
+    }
+
+    override fun off() {
+        heating = false
+    }
+
+    override val isHot: Boolean
+        get() = heating
+}
+
+interface Pump {
+    fun pump()
+}
+
+@ImplBinding
+class Thermosiphon(private val heater: Heater) : Pump {
+    override fun pump() {
+        if (heater.isHot) {
+            println("=> => pumping => =>")
+        }
+    }
 }
