607d238065e30cc14d65b0e8c307714012bc3b9e
ivianuu <ivianuu@gmail.com>
2020-10-07 23:43:43 +0200

    Fixed viewmodel extensions

--- injekt-android/src/main/kotlin/com/ivianuu/injekt/android/ViewModel.kt
+++ injekt-android/src/main/kotlin/com/ivianuu/injekt/android/ViewModel.kt
@@ -1,47 +1,59 @@
 package com.ivianuu.injekt.android
 
-/**
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStoreOwner
-import com.ivianuu.injekt.Effect
-import com.ivianuu.injekt.Given
-import com.ivianuu.injekt.GivenSet
-import com.ivianuu.injekt.Reader
-import com.ivianuu.injekt.given
+import com.ivianuu.injekt.Assisted
+import com.ivianuu.injekt.FunBinding
+import com.ivianuu.injekt.Module
+import com.ivianuu.injekt.merge.BindingModule
 import kotlin.reflect.KClass
 
-@Effect
-annotation class GivenActivityViewModel {
-    @GivenSet
-    companion object {
-        @Given
-        inline fun <reified VM : S, reified S : ViewModel> viewModel(): S =
-            getViewModel<VM, S, ActivityViewModelStoreOwner>(S::class)
+@BindingModule(ActivityComponent::class)
+annotation class ActivityViewModelBinding {
+    @Module
+    class ModuleImpl<VM : S, S : ViewModel>(
+        private val viewModelClass: KClass<S>
+    ) {
+        @Module
+        fun viewModel(getViewModel: getViewModel<VM, S, ActivityViewModelStoreOwner>): S =
+            getViewModel(viewModelClass)
+
+        companion object {
+            inline operator fun <reified VM : S, reified S : ViewModel> invoke() =
+                ModuleImpl<VM, S>(S::class)
+        }
     }
 }
 
-@Effect
-annotation class GivenFragmentViewModel {
-    @GivenSet
-    companion object {
-        @Given
-        inline fun <reified VM : S, reified S : ViewModel> viewModel(): S =
-            getViewModel<VM, S, FragmentViewModelStoreOwner>(S::class)
+@BindingModule(FragmentComponent::class)
+annotation class FragmentViewModelBinding {
+    @Module
+    class ModuleImpl<VM : S, S : ViewModel>(
+        private val viewModelClass: KClass<S>
+    ) {
+        @Module
+        fun viewModel(getViewModel: getViewModel<VM, S, FragmentViewModelStoreOwner>): S =
+            getViewModel(viewModelClass)
+
+        companion object {
+            inline operator fun <reified VM : S, reified S : ViewModel> invoke() =
+                ModuleImpl<VM, S>(S::class)
+        }
     }
 }
 
-@PublishedApi
-@Reader
-internal fun <VM : S, S : ViewModel, VMS : ViewModelStoreOwner> getViewModel(
-    viewModelClass: KClass<S>
+@FunBinding
+fun <VM : S, S : ViewModel, VMSO : ViewModelStoreOwner> getViewModel(
+    viewModelStoreOwner: VMSO,
+    viewModelFactory: () -> VM,
+    viewModelClass: @Assisted KClass<S>
 ): S {
     return ViewModelProvider(
-        given<VMS>(),
+        viewModelStoreOwner,
         object : ViewModelProvider.Factory {
-            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-                return given<VM>() as T
-            }
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T =
+                viewModelFactory() as T
         }
     )[viewModelClass.java]
-}*/
+}
