3824879d80c66f62cc99ba53cad310e0c28b9536
ivianuu <ivianuu@gmail.com>
2020-11-13 16:38:31 +0100

    Moved couple tests

---
+++ integration-tests/src/main/kotlin/com/ivianuu/injekt/integrationtests/ProviderTest.kt
@@ -0,0 +1,66 @@
+package com.ivianuu.injekt.integrationtests
+
+import com.ivianuu.injekt.test.codegen
+import com.ivianuu.injekt.test.invokeSingleFile
+import org.junit.Test
+
+class ProviderTest {
+
+    @Test
+    fun testProvider() = codegen(
+        """
+            @Component
+            abstract class ProviderComponent {
+                abstract val fooFactory: () -> Foo
+                @Binding
+                protected fun foo() = Foo()
+            }
+
+            fun invoke() {
+                component<ProviderComponent>().fooFactory()
+            }
+        """
+    ) {
+        invokeSingleFile()
+    }
+
+    @Test
+    fun testSuspendProvider() = codegen(
+        """
+            @Component
+            abstract class ProviderComponent {
+                abstract val fooFactory: suspend () -> Foo
+                @Binding
+                protected suspend fun foo() = Foo()
+            }
+
+            fun invoke() {
+                runBlocking {
+                    component<ProviderComponent>().fooFactory()
+                }
+            }
+        """
+    ) {
+        invokeSingleFile()
+    }
+
+    @Test
+    fun testComposableProvider() = codegen(
+        """
+            @Component
+            abstract class ProviderComponent {
+                abstract val fooFactory: @Composable () -> Foo
+                @Composable
+                @Binding
+                protected fun foo() = Foo()
+            }
+
+            fun invoke() {
+                component<ProviderComponent>().fooFactory
+            }
+        """
+    ) {
+        invokeSingleFile()
+    }
+
+}
