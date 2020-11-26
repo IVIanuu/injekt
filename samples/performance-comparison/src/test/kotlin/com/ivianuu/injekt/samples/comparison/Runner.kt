7fb08bd99172cd8b79a28d70deca6b0fefbb0ef4
ivianuu <ivianuu@gmail.com>
2020-11-26 16:00:51 +0100

    Tmp workaround

---
+++ samples/performance-comparison/src/test/kotlin/com/ivianuu/injekt/samples/comparison/Runner.kt
@@ -0,0 +1,13 @@
+package com.ivianuu.injekt.samples.comparison
+
+import com.ivianuu.injekt.samples.comparison.base.runAllInjectionTests
+import org.junit.Test
+
+class Runner {
+
+    @Test
+    fun run() {
+        runAllInjectionTests()
+    }
+
+}
