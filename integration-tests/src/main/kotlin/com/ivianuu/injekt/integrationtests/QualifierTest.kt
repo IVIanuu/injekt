58b5eb55295a1853c32acef264fa6b502f981656
ivianuu <ivianuu@gmail.com>
2020-12-10 17:21:09 +0100

    Added qualifier test

---
+++ integration-tests/src/main/kotlin/com/ivianuu/injekt/integrationtests/QualifierTest.kt
@@ -0,0 +1,96 @@
+package com.ivianuu.injekt.integrationtests
+
+import com.ivianuu.injekt.test.Foo
+import com.ivianuu.injekt.test.codegen
+import com.ivianuu.injekt.test.invokeSingleFile
+import junit.framework.Assert.assertNotSame
+import org.junit.Test
+
+class QualifierTest {
+
+    @Test
+    fun testDistinctQualifier() = codegen(
+        """
+            @Target(AnnotationTarget.TYPE)
+            @Qualifier
+            annotation class MyQualifier
+
+            @Given val foo = Foo()
+            @Given val qualifiedFoo: @MyQualifier Foo = Foo()
+       
+            fun invoke(): Pair<Foo, Foo> {
+                return given<Foo>() to given<@MyQualifier Foo>()
+            }
+            """
+    ) {
+        val (foo1, foo2) = invokeSingleFile<Pair<Foo, Foo>>()
+        assertNotSame(foo1, foo2)
+    }
+
+    @Test
+    fun testDistinctQualifierAnnotationWithArguments() = codegen(
+        """
+            @Target(AnnotationTarget.TYPE)
+            @Qualifier
+            annotation class MyQualifier(val value: String)
+            
+            @Given val foo1: @MyQualifier("a") Foo = Foo()
+            @Given val foo2: @MyQualifier("b") Foo = Foo()
+       
+            fun invoke(): Pair<Foo, Foo> {
+                return given<@MyQualifier("a") Foo>() to given<@MyQualifier("b") Foo>()
+            }
+            """
+    ) {
+        val (foo1, foo2) = invokeSingleFile<Pair<Foo, Foo>>()
+        assertNotSame(foo1, foo2)
+    }
+
+    @Test
+    fun testDistinctQualifierAnnotationWithTypeArguments() = codegen(
+        """
+            @Target(AnnotationTarget.TYPE)
+            @Qualifier
+            annotation class MyQualifier<T>
+            
+            @Given val foo1: @MyQualifier<String> Foo = Foo()
+            @Given val foo2: @MyQualifier<Int> Foo = Foo()
+       
+            fun invoke(): Pair<Foo, Foo> {
+                return given<@MyQualifier<String> Foo>() to given<@MyQualifier<Int> Foo>()
+            }
+            """
+    ) {
+        val (foo1, foo2) = invokeSingleFile<Pair<Foo, Foo>>()
+        assertNotSame(foo1, foo2)
+    }
+
+    @Test
+    fun testTypeParameterWithQualifierUpperBound() = codegen(
+        """
+            @Target(AnnotationTarget.TYPE)
+            @Qualifier
+            annotation class MyQualifier
+            
+            @Given class Dep<T>(val value: @Given @MyQualifier T = given)
+            
+            @Given fun qualified(): @MyQualifier String = ""
+            
+            fun invoke() = given<Dep<String>>()
+            """
+    )
+
+    @Test
+    fun testQualifierWithFunctionTypeParameter() = codegen(
+        """
+            @Target(AnnotationTarget.TYPE)
+            @Qualifier
+            annotation class MyQualifier<T>
+            
+            @Given fun <T> qualifiedFoo(): @MyQualifier<T> Foo = Foo()
+             
+            fun invoke() = given<@MyQualifier<String> Foo>()
+            """
+    )
+
+}
