12871c1ad5feab35158041882e6dd9e5cd23507f
ivianuu <ivianuu@gmail.com>
2020-12-11 22:00:08 +0100

    Suppress compose error

--- injekt-compiler-plugin/src/main/kotlin/com/ivianuu/injekt/compiler/InjektComponentRegistrar.kt
+++ injekt-compiler-plugin/src/main/kotlin/com/ivianuu/injekt/compiler/InjektComponentRegistrar.kt
@@ -20,11 +20,13 @@ import com.google.auto.service.AutoService
 import com.ivianuu.injekt.compiler.transform.InjektIrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.com.intellij.mock.MockProject
+import org.jetbrains.kotlin.com.intellij.openapi.extensions.Extensions
 import org.jetbrains.kotlin.com.intellij.openapi.extensions.LoadingOrder
 import org.jetbrains.kotlin.compiler.plugin.ComponentRegistrar
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.config.JVMConfigurationKeys
 import org.jetbrains.kotlin.extensions.StorageComponentContainerContributor
+import org.jetbrains.kotlin.resolve.diagnostics.DiagnosticSuppressor
 import org.jetbrains.kotlin.resolve.jvm.extensions.AnalysisHandlerExtension
 import java.io.File
 
@@ -57,6 +59,9 @@ class InjektComponentRegistrar : ComponentRegistrar {
                 project,
                 InjektIrGenerationExtension(DeclarationStore())
             )
+            @Suppress("DEPRECATION")
+            Extensions.getRootArea().getExtensionPoint(DiagnosticSuppressor.EP_NAME)
+                .registerExtension(InjektDiagnosticSupressor())
         }
     }
 }
---
+++ injekt-compiler-plugin/src/main/kotlin/com/ivianuu/injekt/compiler/InjektDiagnosticSupressor.kt
@@ -0,0 +1,22 @@
+package com.ivianuu.injekt.compiler
+
+import org.jetbrains.kotlin.diagnostics.Diagnostic
+import org.jetbrains.kotlin.diagnostics.DiagnosticWithParameters2
+import org.jetbrains.kotlin.diagnostics.Severity
+import org.jetbrains.kotlin.resolve.BindingContext
+import org.jetbrains.kotlin.resolve.diagnostics.DiagnosticSuppressor
+import org.jetbrains.kotlin.types.KotlinType
+import org.jetbrains.kotlin.types.typeUtil.isNothing
+
+class InjektDiagnosticSupressor : DiagnosticSuppressor {
+
+    override fun isSuppressed(diagnostic: Diagnostic): Boolean =
+        isSuppressed(diagnostic, null)
+
+    override fun isSuppressed(diagnostic: Diagnostic, bindingContext: BindingContext?): Boolean {
+        return diagnostic is DiagnosticWithParameters2<*, *, *> &&
+                diagnostic.severity == Severity.ERROR &&
+                (diagnostic.b.let { it is KotlinType && it.isNothing() }) &&
+                diagnostic.psiElement.text == "given"
+    }
+}
