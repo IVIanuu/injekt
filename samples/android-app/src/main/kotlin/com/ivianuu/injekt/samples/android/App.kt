faab1f6723f011bf6233e5a9cc3b016af740adb5
IVIanuu <IVIanuu@gmail.com>
2020-12-22 12:40:07 +0100

    Fix sample

--- samples/android-app/src/main/kotlin/com/ivianuu/injekt/samples/android/App.kt
+++ samples/android-app/src/main/kotlin/com/ivianuu/injekt/samples/android/App.kt
@@ -17,11 +17,16 @@
 package com.ivianuu.injekt.samples.android
 
 import android.app.Application
+import com.ivianuu.injekt.component.applicationComponent
 import com.ivianuu.injekt.component.initializeApp
+import com.ivianuu.injekt.withGiven
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         initializeApp()
+        withGiven(applicationComponent) {
+            initializeWorkers()
+        }
     }
 }
