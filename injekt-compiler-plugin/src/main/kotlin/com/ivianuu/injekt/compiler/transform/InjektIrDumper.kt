43a96c569d6a683c0611aa6716eea4094ade87dc
manu <ivianuu@gmail.com>
2021-02-26 14:29:27 +0100

    Use dumpKotlinLike for ir dumper and removed IrSourcePrinter

--- injekt-compiler-plugin/src/main/kotlin/com/ivianuu/injekt/compiler/transform/InjektIrDumper.kt
+++ injekt-compiler-plugin/src/main/kotlin/com/ivianuu/injekt/compiler/transform/InjektIrDumper.kt
@@ -24,6 +24,10 @@ import com.ivianuu.injekt.compiler.index.CliIndexStore
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
+import org.jetbrains.kotlin.ir.util.FakeOverridesStrategy
+import org.jetbrains.kotlin.ir.util.KotlinLikeDumpOptions
+import org.jetbrains.kotlin.ir.util.LabelPrintingStrategy
+import org.jetbrains.kotlin.ir.util.dumpKotlinLike
 import java.io.File
 
 class InjektIrDumper(
@@ -38,7 +42,12 @@ class InjektIrDumper(
                 it.fqName,
                 file.name.removeSuffix(".kt"),
                 file.absolutePath,
-                it.dumpSrc(DeclarationStore(CliIndexStore(pluginContext.moduleDescriptor), pluginContext.moduleDescriptor))
+                it.dumpKotlinLike(
+                    KotlinLikeDumpOptions(
+                        useNamedArguments = true,
+                        printFakeOverridesStrategy = FakeOverridesStrategy.NONE
+                    )
+                )
             )
         }
         fileManager.postGenerate()
