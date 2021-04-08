b331df924c8fc3cf2756e791facda3d7bff99529
manu <ivianuu@gmail.com>
2021-04-08 22:41:25 +0200

    More extensions

--- injekt-ktor/src/jvmMain/kotlin/com/ivianuu/injekt/ktor/App.kt
+++ injekt-ktor/src/jvmMain/kotlin/com/ivianuu/injekt/ktor/App.kt
@@ -5,11 +5,19 @@ import io.ktor.application.Application
 import io.ktor.util.AttributeKey
 import com.ivianuu.injekt.scope.AppGivenScope
 import com.ivianuu.injekt.scope.GivenScopeElementBinding
+import io.ktor.application.ApplicationCall
 import io.ktor.application.ApplicationStopped
+import io.ktor.routing.Routing
 
 val Application.appGivenScope: AppGivenScope
     get() = attributes.getOrNull(AppGivenScopeKey)
         ?: error("No app given scope found. Did you forget to call Application.initializeAppGivenScope?")
+
+val Routing.appGivenScope: AppGivenScope
+    get() = application.appGivenScope
+
+val ApplicationCall.appGivenScope: AppGivenScope
+    get() = application.appGivenScope
 
 fun Application.initializeAppGivenScope(
     @Given scopeFactory: (@Given @GivenScopeElementBinding<AppGivenScope> Application) -> AppGivenScope
