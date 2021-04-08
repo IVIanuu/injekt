1e0ce5d0ae5988123e18abb6ffb90b3195317a9d
manu <ivianuu@gmail.com>
2021-04-08 18:17:52 +0200

    Do not add a given for each android system service and instead ad a single given for @SystemService T

---
+++ injekt-android/src/test/kotlin/com/ivianuu/injekt/android/SystemServiceTest.kt
@@ -0,0 +1,29 @@
+package com.ivianuu.injekt.android
+
+import android.os.PowerManager
+import androidx.test.core.app.ActivityScenario
+import com.ivianuu.injekt.Given
+import com.ivianuu.injekt.scope.GivenScopeElementBinding
+import com.ivianuu.injekt.scope.element
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@Config(sdk = [28])
+@RunWith(RobolectricTestRunner::class)
+class SystemServiceTest {
+    @Test
+    fun testCanRequestSystemService() {
+        val scenario = ActivityScenario.launch(AndroidTestActivity::class.java)
+        scenario.onActivity {
+            it.activityGivenScope.element<SystemServiceComponent>()
+                .powerManager
+                .isPowerSaveMode
+        }
+    }
+}
+
+@Given
+@GivenScopeElementBinding<ActivityGivenScope>
+class SystemServiceComponent(@Given val powerManager: @SystemService PowerManager)
