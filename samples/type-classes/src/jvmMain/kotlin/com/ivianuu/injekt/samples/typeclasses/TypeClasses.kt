ee9db805695aa6a24bb4a68c1819841e1950f4de
manu <ivianuu@gmail.com>
2021-05-10 23:13:13 +0200

    Renamed function

--- samples/type-classes/src/jvmMain/kotlin/com/ivianuu/injekt/samples/typeclasses/TypeClasses.kt
+++ samples/type-classes/src/jvmMain/kotlin/com/ivianuu/injekt/samples/typeclasses/TypeClasses.kt
@@ -17,18 +17,20 @@
 package com.ivianuu.injekt.samples.typeclasses
 
 import com.ivianuu.injekt.*
-import com.ivianuu.injekt.common.*
 
-@Extension
-fun interface Ord<in T> {
-    infix fun T.compareWith(other: T): Int
+interface Ord<in T> {
+    fun compareWith(a: T, b: T): Int
 }
+
+infix fun <T> T.compareWith(other: T, @Given ord: Ord<T>): Int = ord.compareWith(this, other)
 
 fun <T> List<T>.ordered(@Given ord: Ord<T>): List<T> =
     sortedWith { a, b -> a compareWith b }
 
 @Given
-val IntOrd = Ord<Int> { compareTo(it) }
+object IntOrd : Ord<Int> {
+    override fun compareWith(a: Int, b: Int): Int = a.compareTo(b)
+}
 
 fun main() {
     val items = listOf(5, 3, 4, 1, 2).ordered()
