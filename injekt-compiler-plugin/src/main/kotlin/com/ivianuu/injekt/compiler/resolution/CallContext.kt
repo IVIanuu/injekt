a703bf68feee2a663459e530bfa488d56ab431a8
manu <ivianuu@gmail.com>
2021-06-01 16:59:18 +0200

    Fix the call context value parameters call context could be non default

--- injekt-compiler-plugin/src/main/kotlin/com/ivianuu/injekt/compiler/resolution/CallContext.kt
+++ injekt-compiler-plugin/src/main/kotlin/com/ivianuu/injekt/compiler/resolution/CallContext.kt
@@ -38,9 +38,14 @@ fun CallContext.canCall(other: CallContext) =
   this == other || other == CallContext.DEFAULT
 
 fun CallableDescriptor.callContext(bindingContext: BindingContext): CallContext {
+  if (this !is FunctionDescriptor && this !is PropertyDescriptor)
+    return CallContext.DEFAULT
+
   if (this is ConstructorDescriptor) return CallContext.DEFAULT
+
   if (composeCompilerInClasspath && isComposableCallable(bindingContext))
     return CallContext.COMPOSABLE
+
   var node: PsiElement? = findPsi()
   if (node == null) return callContextOfThis
   try {
