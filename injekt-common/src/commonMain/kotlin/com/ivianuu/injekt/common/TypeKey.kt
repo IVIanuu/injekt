b231db239d765255b9cf2f78650fb878ace89be2
manu <ivianuu@gmail.com>
2021-06-02 16:56:42 +0200

    Added typeKeyOf<T>() function

--- injekt-common/src/commonMain/kotlin/com/ivianuu/injekt/common/TypeKey.kt
+++ injekt-common/src/commonMain/kotlin/com/ivianuu/injekt/common/TypeKey.kt
@@ -16,8 +16,16 @@
 
 package com.ivianuu.injekt.common
 
+import com.ivianuu.injekt.*
+
 /**
  * A key for a injekt type which can be used as a map key or similar
  */
 @Suppress("EXPERIMENTAL_FEATURE_WARNING")
 inline class TypeKey<out T>(val value: String)
+
+/**
+ * Returns the [TypeKey] of [T]
+ */
+@Suppress("NOTHING_TO_INLINE")
+inline fun <T> typeKeyOf(@Inject key: TypeKey<T>): TypeKey<T> = key
