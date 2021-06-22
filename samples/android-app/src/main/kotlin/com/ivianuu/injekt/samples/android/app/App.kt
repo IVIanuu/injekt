a6f7e217520cf6bc346646c3363df87337065858
manu <ivianuu@gmail.com>
2021-06-22 15:27:27 +0200

    Lol:)

--- samples/android-app/src/main/kotlin/com/ivianuu/injekt/samples/android/app/App.kt
+++ samples/android-app/src/main/kotlin/com/ivianuu/injekt/samples/android/app/App.kt
@@ -25,18 +25,7 @@ package com.ivianuu.injekt.samples.android.app
 import android.app.*
 import com.ivianuu.injekt.*
 import com.ivianuu.injekt.android.*
-import com.ivianuu.injekt.scope.*
-import kotlinx.coroutines.*
-import kotlin.coroutines.*
 
 class App : Application(), AppAmbientsOwner {
   override val appAmbients by lazy { createAppAmbients() }
-
-  override fun onCreate() {
-    super.onCreate()
-    withScope {
-      val coroutineScope = CoroutineScope(EmptyCoroutineContext).bind()
-
-    }
-  }
 }
