6857baadfa5424192e799f2eb278cfd9caeecd0d
manu <ivianuu@gmail.com>
2021-07-02 13:03:58 +0200

    Updated compose

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -31,13 +31,13 @@ object Deps {
 
   object AndroidX {
     object Activity {
-      private const val version = "1.3.0-beta02"
+      private const val version = "1.3.0-rc01"
       const val activity = "androidx.activity:activity:$version"
       const val compose = "androidx.activity:activity-compose:$version"
     }
 
     object Compose {
-      const val version = "1.0.0-beta09"
+      const val version = "1.0.0-rc01"
       const val compiler = "androidx.compose.compiler:compiler:$version"
       const val material = "androidx.compose.material:material:$version"
       const val runtime = "androidx.compose.runtime:runtime:$version"
