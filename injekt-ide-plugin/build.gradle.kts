0b82b2f879cceaed262a5af39cc254ec1e10ceca
manu <ivianuu@gmail.com>
2021-08-09 14:55:33 +0200

    ...

--- injekt-ide-plugin/build.gradle.kts
+++ injekt-ide-plugin/build.gradle.kts
@@ -18,7 +18,7 @@ import org.jetbrains.intellij.tasks.*
 
 plugins {
   kotlin("jvm")
-  id("org.jetbrains.intellij") version "1.1.2"
+  id("org.jetbrains.intellij") version "1.1.4"
 }
 
 apply(from = "https://raw.githubusercontent.com/IVIanuu/gradle-scripts/master/java-8.gradle")
@@ -29,12 +29,18 @@ intellij {
   pluginName.set("Injekt ide plugin")
   updateSinceUntilBuild.set(false)
   plugins.addAll("org.jetbrains.kotlin:203-1.5.21-release-328-AS7717.8", "gradle", "gradle-java", "java")
-  localPath.set("/home/manu/android-studio")
+  //localPath.set("/home/manu/android-studio")
 }
 
 tasks {
   instrumentCode {
     compilerVersion.set("201.7846.76")
+  }
+  runIde {
+    jbrVersion.set("11_0_3b360.2")
+  }
+  buildSearchableOptions {
+    jbrVersion.set("11_0_3b360.2")
   }
 }
 
