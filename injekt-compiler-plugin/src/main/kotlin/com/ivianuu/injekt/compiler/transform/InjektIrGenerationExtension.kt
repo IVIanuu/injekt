2845df95bf578c6e31fba3d76c5695dde48603a9
manu <IVIanuu@gmail.com>
2021-10-17 21:48:36 +0200

    ...

--- injekt-compiler-plugin/src/main/kotlin/com/ivianuu/injekt/compiler/transform/InjektIrGenerationExtension.kt
+++ injekt-compiler-plugin/src/main/kotlin/com/ivianuu/injekt/compiler/transform/InjektIrGenerationExtension.kt
@@ -18,17 +18,25 @@ package com.ivianuu.injekt.compiler.transform
 
 import com.ivianuu.injekt.compiler.InjektContext
 import com.ivianuu.injekt.compiler.InjektFqNames
+import com.ivianuu.injekt.compiler.InjektWritableSlices
 import com.ivianuu.injekt_shaded.Inject
 import com.ivianuu.injekt_shaded.Provide
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.ObsoleteDescriptorBasedAPI
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
+import org.jetbrains.kotlin.ir.util.FakeOverridesStrategy
+import org.jetbrains.kotlin.ir.util.KotlinLikeDumpOptions
+import org.jetbrains.kotlin.ir.util.dumpKotlinLike
 import org.jetbrains.kotlin.ir.util.patchDeclarationParents
 import org.jetbrains.kotlin.resolve.DelegatingBindingTrace
+import java.io.File
+
+var dumpAllFiles = false
 
 @OptIn(ObsoleteDescriptorBasedAPI::class)
 class InjektIrGenerationExtension(
+  private val dumpDir: File,
   @Inject private val injektFqNames: InjektFqNames
 ) : IrGenerationExtension {
   override fun generate(moduleFragment: IrModuleFragment, @Provide pluginContext: IrPluginContext) {
@@ -39,5 +47,41 @@ class InjektIrGenerationExtension(
     )
     moduleFragment.transform(InjectCallTransformer(), null)
     moduleFragment.patchDeclarationParents()
+    moduleFragment.dumpToFiles(dumpDir, pluginContext)
   }
+}
+
+@OptIn(ObsoleteDescriptorBasedAPI::class) fun IrModuleFragment.dumpToFiles(
+  dumpDir: File,
+  pluginContext: IrPluginContext
+) {
+  files
+    .filter {
+      dumpAllFiles || pluginContext.bindingContext[InjektWritableSlices.INJECTIONS_OCCURRED_IN_FILE,
+          it.fileEntry.name] != null
+    }
+    .forEach { irFile ->
+      val file = File(irFile.fileEntry.name)
+      val content = try {
+        irFile.dumpKotlinLike(
+          KotlinLikeDumpOptions(
+            useNamedArguments = true,
+            printFakeOverridesStrategy = FakeOverridesStrategy.NONE
+          )
+        )
+      } catch (e: Throwable) {
+        e.stackTraceToString()
+      }
+      val newFile = dumpDir
+        .resolve(irFile.fqName.asString().replace(".", "/"))
+        .also { it.mkdirs() }
+        .resolve(file.name.removeSuffix(".kt"))
+      try {
+        newFile.createNewFile()
+        newFile.writeText(content)
+        println("Generated $newFile:\n$content")
+      } catch (e: Throwable) {
+        throw RuntimeException("Failed to create file ${newFile.absolutePath}\n$content")
+      }
+    }
 }
