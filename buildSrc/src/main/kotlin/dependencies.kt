9bf6f438a42967e35aa34ab85284ea756b167394
manu <IVIanuu@gmail.com>
2021-11-04 00:30:12 +0100

    ...

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -69,7 +69,7 @@ object Deps {
   const val dokkaGradlePlugin = "org.jetbrains.dokka:dokka-gradle-plugin:1.4.20"
 
   const val injektGradlePlugin = "com.ivianuu.injekt:injekt-gradle-plugin:0.0.1-dev646"
-  const val injektGradlePluginShaded = "com.ivianuu.injekt:injekt-gradle-plugin-shaded:0.0.1-dev643"
+  const val injektGradlePluginShaded = "com.ivianuu.injekt:injekt-gradle-plugin-shaded:0.0.1-dev646"
 
   const val junit = "junit:junit:4.12"
 
