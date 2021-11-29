fdb6456a8e986ab27d381da141f30a3460bbc657
manu <IVIanuu@gmail.com>
2021-11-30 00:48:29 +0100

    More performant scope implementation

--- injekt-common/src/commonMain/kotlin/com/ivianuu/injekt/common/Scope.kt
+++ injekt-common/src/commonMain/kotlin/com/ivianuu/injekt/common/Scope.kt
@@ -20,22 +20,54 @@ import com.ivianuu.injekt.Inject
 import com.ivianuu.injekt.Provide
 import com.ivianuu.injekt.Spread
 import com.ivianuu.injekt.Tag
-import kotlinx.atomicfu.atomic
 import kotlinx.atomicfu.locks.reentrantLock
 import kotlinx.atomicfu.locks.withLock
 
 interface Scope<N> : Disposable {
-  operator fun <T : Any> invoke(@Inject key: TypeKey<T>, init: () -> T): T
+  @InternalScopeApi fun lock()
+
+  @InternalScopeApi fun unlock()
+
+  @InternalScopeApi fun get(key: Any): Any?
+
+  @InternalScopeApi fun set(key: Any, value: Any)
 }
 
 fun <N> Scope(): Scope<N> = ScopeImpl()
 
+@OptIn(InternalScopeApi::class)
+@Suppress("UNCHECKED_CAST")
+inline operator fun <T : Any> Scope<*>.invoke(@Inject key: TypeKey<T>, init: () -> T): T {
+  get(key.value)?.let { return it as T }
+  return try {
+    lock()
+    get(key.value)
+      ?.let { it as T }
+      ?: init()
+        .also { set(key.value, it) }
+  } finally {
+    unlock()
+  }
+}
+
+@RequiresOptIn(level = RequiresOptIn.Level.ERROR)
+annotation class InternalScopeApi
+
+@OptIn(InternalScopeApi::class)
 private class ScopeImpl<N> : Scope<N>, Disposable {
-  private val values = mutableMapOf<String, Any>()
+  private val values = mutableMapOf<Any, Any>()
+
   private val lock = reentrantLock()
 
-  override fun <T : Any> invoke(@Inject key: TypeKey<T>, init: () -> T): T =
-    lock.withLock { values.getOrPut(key.value, init) as T }
+  override fun lock() = lock.lock()
+
+  override fun unlock() = lock.unlock()
+
+  override fun get(key: Any): Any? = values[key]
+
+  override fun set(key: Any, value: Any) {
+    values[key] = value
+  }
 
   override fun dispose() {
     lock.withLock {
@@ -49,7 +81,7 @@ private class ScopeImpl<N> : Scope<N>, Disposable {
 
 @Tag annotation class Scoped<N> {
   companion object {
-    @Provide fun <@Spread T : @Scoped<N> S, S : Any, N> scoped(
+    @Provide inline fun <@Spread T : @Scoped<N> S, S : Any, N> scoped(
       init: () -> T,
       scope: Scope<N>,
       key: TypeKey<S>
