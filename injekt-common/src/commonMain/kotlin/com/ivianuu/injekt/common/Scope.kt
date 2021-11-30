eea8a259f4fd57781942407643672bb132993844
manu <IVIanuu@gmail.com>
2021-11-30 21:22:33 +0100

    Simplified Scope api

--- injekt-common/src/commonMain/kotlin/com/ivianuu/injekt/common/Scope.kt
+++ injekt-common/src/commonMain/kotlin/com/ivianuu/injekt/common/Scope.kt
@@ -8,50 +8,18 @@ import com.ivianuu.injekt.*
 import kotlinx.atomicfu.locks.*
 
 interface Scope<N> : Disposable {
-  @InternalScopeApi fun lock()
-
-  @InternalScopeApi fun unlock()
-
-  @InternalScopeApi fun get(key: Any): Any?
-
-  @InternalScopeApi fun set(key: Any, value: Any)
+  operator fun <T : Any> invoke(@Inject key: TypeKey<T>, init: () -> T): T
 }
 
 fun <N> Scope(): Scope<N> = ScopeImpl()
 
-@OptIn(InternalScopeApi::class)
-@Suppress("UNCHECKED_CAST")
-inline operator fun <T : Any> Scope<*>.invoke(@Inject key: TypeKey<T>, init: () -> T): T {
-  get(key.value)?.let { return it as T }
-  lock()
-  return try {
-    get(key.value)
-      ?.let { it as T }
-      ?: init()
-        .also { set(key.value, it) }
-  } finally {
-    unlock()
-  }
-}
-
-@RequiresOptIn(level = RequiresOptIn.Level.ERROR)
-annotation class InternalScopeApi
-
-@OptIn(InternalScopeApi::class)
 private class ScopeImpl<N> : Scope<N>, Disposable {
   private val values = mutableMapOf<Any, Any>()
 
   private val lock = reentrantLock()
 
-  override fun lock() = lock.lock()
-
-  override fun unlock() = lock.unlock()
-
-  override fun get(key: Any): Any? = values[key]
-
-  override fun set(key: Any, value: Any) {
-    values[key] = value
-  }
+  override fun <T : Any> invoke(@Inject key: TypeKey<T>, init: () -> T): T =
+    lock.withLock { values.getOrPut(key.value, init) as T }
 
   override fun dispose() {
     lock.withLock {
@@ -65,10 +33,11 @@ private class ScopeImpl<N> : Scope<N>, Disposable {
 
 @Tag annotation class Scoped<N> {
   companion object {
-    @Provide inline fun <@Spread T : @Scoped<N> S, S : Any, N> scoped(
+    @Provide fun <@Spread T : @Scoped<N> S, S : Any, N> scoped(
       init: () -> T,
       scope: Scope<N>,
       key: TypeKey<S>
     ): S = scope(key, init)
   }
 }
+
