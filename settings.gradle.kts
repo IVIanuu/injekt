86f5b5e6a2ead2c6e89ae952d8732851851b1fec
manu <IVIanuu@gmail.com>
2022-01-10 12:26:16 +0100

    Updated copyright

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Manuel Wrage. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2022 Manuel Wrage. Use of this source code is governed by the Apache 2.0 license.
  */
 
 include(
