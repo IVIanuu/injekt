75bf46f8781647bb60ef6d2d8ad1e0f4db1d8438
manu <ivianuu@gmail.com>
2023-07-14 10:22:35 +0200

    ...

--- core/src/commonMain/kotlin/com/ivianuu/injekt/Api.kt
+++ core/src/commonMain/kotlin/com/ivianuu/injekt/Api.kt
@@ -6,6 +6,8 @@
 
 package com.ivianuu.injekt
 
+import com.ivianuu.injekt.Providers.Empty
+
 /**
  * Provides this declaration in the current scope
  */
@@ -35,61 +37,6 @@ package com.ivianuu.injekt
 annotation class Provide
 
 inline fun <F : Function<*>> provider(f: F): @Provide F = f
-
-inline fun <A, R> provide(a: A, block: (@Provide A) -> R): R = block(a)
-
-@Provide data class Providers2<A, B>(
-  @property:Provide val _a: A,
-  @property:Provide val _b: B
-)
-
-inline fun <A, B, R> provide(a: A, b: B, block: (@Provide Providers2<A, B>) -> R): R =
-  block(Providers2(a, b))
-
-@Provide data class Providers3<A, B, C>(
-  @property:Provide val _a: A,
-  @property:Provide val _b: B,
-  @property:Provide val _c: C
-)
-
-inline fun <A, B, C, R> provide(
-  a: A,
-  b: B,
-  c: C,
-  block: (@Provide Providers3<A, B, C>) -> R
-): R = block(Providers3(a, b, c))
-
-@Provide data class Providers4<A, B, C, D>(
-  @property:Provide val _a: A,
-  @property:Provide val _b: B,
-  @property:Provide val _c: C,
-  @property:Provide val _D: D
-)
-
-inline fun <A, B, C, D, R> provide(
-  a: A,
-  b: B,
-  c: C,
-  d: D,
-  block: (@Provide Providers4<A, B, C, D>) -> R
-): R = block(Providers4(a, b, c, d))
-
-@Provide data class Providers5<A, B, C, D, E>(
-  @property:Provide val _a: A,
-  @property:Provide val _b: B,
-  @property:Provide val _c: C,
-  @property:Provide val _D: D,
-  @property:Provide val _e: E
-)
-
-inline fun <A, B, C, D, E, R> provide(
-  a: A,
-  b: B,
-  c: C,
-  d: D,
-  e: E,
-  block: (@Provide Providers5<A, B, C, D, E>) -> R
-): R = block(Providers5(a, b, c, d, e))
 
 /**
  * Automatically fills in a argument if no explicit argument was provided
@@ -140,3 +87,102 @@ annotation class Tag
  */
 @Target(AnnotationTarget.TYPE_PARAMETER)
 annotation class Spread
+
+inline fun <A, R> provide(@Provide a: A, block: (@Provide Providers1<A>) -> R): R = block(inject())
+
+inline fun <A, B, R> provide(@Provide a: A, @Provide b: B, block: (@Provide Providers2<A, B>) -> R): R =
+  block(inject())
+
+inline fun <A, B, C, R> provide(
+  @Provide a: A,
+  @Provide b: B,
+  @Provide c: C,
+  block: (@Provide Providers3<A, B, C>) -> R
+): R = block(inject())
+
+inline fun <A, B, C, D, R> provide(
+  @Provide a: A,
+  @Provide b: B,
+  @Provide c: C,
+  @Provide d: D,
+  block: (@Provide Providers4<A, B, C, D>) -> R
+): R = block(inject())
+
+inline fun <A, B, C, D, E, R> provide(
+  @Provide a: A,
+  @Provide b: B,
+  @Provide c: C,
+  @Provide d: D,
+  @Provide e: E,
+  block: (@Provide Providers5<A, B, C, D, E>) -> R
+): R = block(inject())
+
+inline fun <A, B, C, D, E, F, R> provide(
+  @Provide a: A,
+  @Provide b: B,
+  @Provide c: C,
+  @Provide d: D,
+  @Provide e: E,
+  @Provide f: F,
+  block: (@Provide Providers6<A, B, C, D, E, F>) -> R
+): R = block(inject())
+
+inline fun <A, B, C, D, E, F, G, R> provide(
+  @Provide a: A,
+  @Provide b: B,
+  @Provide c: C,
+  @Provide d: D,
+  @Provide e: E,
+  @Provide f: F,
+  @Provide g: G,
+  block: (@Provide Providers7<A, B, C, D, E, F, G>) -> R
+): R = block(inject())
+
+inline fun <A, B, C, D, E, F, G, H, R> provide(
+  @Provide a: A,
+  @Provide b: B,
+  @Provide c: C,
+  @Provide d: D,
+  @Provide e: E,
+  @Provide f: F,
+  @Provide g: G,
+  @Provide h: H,
+  block: (@Provide Providers8<A, B, C, D, E, F, G, H>) -> R
+): R = block(inject())
+
+inline fun <A, B, C, D, E, F, G, H, I, R> provide(
+  @Provide a: A,
+  @Provide b: B,
+  @Provide c: C,
+  @Provide d: D,
+  @Provide e: E,
+  @Provide f: F,
+  @Provide g: G,
+  @Provide h: H,
+  @Provide i: I,
+  block: (@Provide Providers9<A, B, C, D, E, F, G, H, I>) -> R
+): R = block(inject())
+
+typealias Providers1<A> = Providers<A, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty>
+typealias Providers2<A, B> = Providers<A, B, Empty, Empty, Empty, Empty, Empty, Empty, Empty>
+typealias Providers3<A, B, C> = Providers<A, B, C, Empty, Empty, Empty, Empty, Empty, Empty>
+typealias Providers4<A, B, C, D> = Providers<A, B, C, D, Empty, Empty, Empty, Empty, Empty>
+typealias Providers5<A, B, C, D, E> = Providers<A, B, C, D, E, Empty, Empty, Empty, Empty>
+typealias Providers6<A, B, C, D, E, F> = Providers<A, B, C, D, E, F, Empty, Empty, Empty>
+typealias Providers7<A, B, C, D, E, F, G> = Providers<A, B, C, D, E, F, G, Empty, Empty>
+typealias Providers8<A, B, C, D, E, F, G, H> = Providers<A, B, C, D, E, F, G, H, Empty>
+typealias Providers9<A, B, C, D, E, F, G, H, I> = Providers<A, B, C, D, E, F, G, H, I>
+
+@Provide data class Providers<A, B, C, D, E, F, G, H, I>(
+  @Inject @property:Provide val a: A,
+  @Inject @property:Provide val b: B,
+  @Inject @property:Provide val c: C,
+  @Inject @property:Provide val d: D,
+  @Inject @property:Provide val e: E,
+  @Inject @property:Provide val f: F,
+  @Inject @property:Provide val g: G,
+  @Inject @property:Provide val h: H,
+  @Inject @property:Provide val i: I
+) {
+  @Provide object Empty
+}
