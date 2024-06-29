365f183e2ddacd0d6de19dbbac75d0837aeb1125
Manu <ivianuu@gmail.com>
2024-06-29 11:23:20 +0200

    ...

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,4 +1,4 @@
-import org.jetbrains.kotlin.gradle.tasks.*
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 /*
  * Copyright 2022 Manuel Wrage. Use of this source code is governed by the Apache 2.0 license.
